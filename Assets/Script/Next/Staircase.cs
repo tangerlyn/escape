b6440761d82550e45e1df40577eb3b53bcc5f40b
Gyulyn Kim <90612408+tangerlyn@users.noreply.github.com>
2025-02-21 22:23:37 +0900

    scene 잘 넘어감

--- Assets/Script/Next/Staircase.cs
+++ Assets/Script/Next/Staircase.cs
@@ -7,8 +7,23 @@ public class Staircase : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("플레이어가 계단에 접근했습니다. Floor2 씬으로 이동합니다.");
-            SceneManager.LoadScene("Second");  
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            
+            if (currentSceneName == "GameScene")
+            {
+                Debug.Log("플레이어가 계단에 접근했습니다. floor1에서 floor2 씬으로 이동합니다.");
+                SceneManager.LoadScene("Second");
+            }
+            else if (currentSceneName == "Second")
+            {
+                Debug.Log("플레이어가 계단에 접근했습니다. floor2에서 floor3 씬으로 이동합니다.");
+                SceneManager.LoadScene("floor3");
+            }
+            else if (currentSceneName == "floor3")
+            {
+                Debug.Log("플레이어가 계단에 접근했습니다. floor3에서 floor4 씬으로 이동합니다.");
+                SceneManager.LoadScene("Fourth");
+            }
         }
     }
 }
--- Assets/Script/Next/Staircase2.cs
+++ Assets/Script/Next/Staircase2.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Staircase1 : MonoBehaviour
+public class Staircase2 : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("플레이어가 계단에 접근했습니다. Floor4 씬으로 이동합니다.");
-            SceneManager.LoadScene("floor3");  
+            SceneManager.LoadScene("Fourth");  
         }
     }
 }
